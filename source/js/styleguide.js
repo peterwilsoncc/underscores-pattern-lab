d718a6f6e773362f085f7ffa3eab18ec6e4bd1e9
Peter Wilson <git-commit-by@peterwilson.cc>
2015-10-18 14:59:23 +1100

    gh-8 Categories Widget
    Various layouts of the category widget (eight total).
    
    resolves gh-8

--- source/js/styleguide.js
+++ source/js/styleguide.js
@@ -1,25 +1,53 @@
 var PWCC = window.PWCC || {};
 
-(PWCC.sg = function( window, undefined ){ 
-	
+(PWCC.sg = function( window, undefined ){
+
 	var $ = window.jQuery;
-	
+
 	var $tagClouds = $( '.widget_tag_cloud > .tagcloud' );
-	
+
 	$tagClouds.each( function() {
 		var $links = $( this ).find( 'a' ),
 		    seperator = $( this ).data( 'styleguide-seperator' ),
 		    totalLinks = $links.length;
-		
+
 		$links.each( function( i ){
 			if ( i + 1 === totalLinks ) {
 				return false;
 			}
-			
+
 			$( this ).after( seperator );
-			
+
 		} );
 	} );
-	
-	
+
+	var $categoryWidgets = $( '.widget_categories' );
+
+	$categoryWidgets.each( function() {
+		var $widget = $( this );
+		var $label = $widget.children( 'label' );
+		var $select = $widget.children( 'select' );
+		var id = 'cat-' + guid();
+
+		$label.attr( 'for', id );
+		$select.attr( 'id', id );
+	} );
+
+
+	function guid(){
+		var characters = '0123456789ABCDEFGHIJKLMNOPQRSTUVWXYZ'.split( '' );
+		var charactersLength = characters.length;
+
+		function S4() {
+			var r = '';
+			var i = 4;
+			for ( ; i>0; i-- ) {
+				r += characters[Math.floor(Math.random()*charactersLength)];
+			}
+			return r;
+		}
+
+		return (S4()+S4()+"-"+S4()+"-"+S4()+"-"+S4()+"-"+S4()+S4()+S4());
+	}
+
 }( window ));
